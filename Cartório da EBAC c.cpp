6a25104c21cada83b4a0556d259b8ecd39c17167
eyshila7 <137560252+eyshila7@users.noreply.github.com>
2023-06-30 15:07:53 -0300

    Update Cartório da EBAC c.cpp

--- Cartório da EBAC c.cpp
+++ Cartório da EBAC c.cpp
@@ -1,26 +1,26 @@
-#include <stdio.h> //biblioteca de comunicaÁ„o com o usu·rio
-#include <stdlib.h>//biblioteca de alocaÁ„p de espaÁo em memÛria
-#include <locale.h>//biblioteca de alocaÁ„o de texto por regi„o
-#include <string.h>//biblioteca respons·vel por cuidar das string
+#include <stdio.h> //biblioteca de comunica√ß√£o com o usu√°rio
+#include <stdlib.h>//biblioteca de aloca√ß√£p de espa√ßo em mem√≥ria
+#include <locale.h>//biblioteca de aloca√ß√£o de texto por regi√£o
+#include <string.h>//biblioteca respons√°vel por cuidar das string
 
-int registro() // FunÁ„o responsavel por cadastrar os usu·rios no sistema 
+int registro() // Fun√ß√£o responsavel por cadastrar os usu√°rios no sistema 
 {
-    //inicio criaÁ„o de vari·veis/string
+    //inicio cria√ß√£o de vari√°veis/string
     char arquivo[40];
     char cpf[40];	
     char nome[40];
     char sobrenome[40];
     char cargo[40];
-     //final da criaÁ„o de vari·veis/string
+     //final da cria√ß√£o de vari√°veis/string
      
-     printf("Digite o CPF a ser cadastrado: ");//  coletaando informaÁ„o do usu·rio
+     printf("Digite o CPF a ser cadastrado: ");//  coletaando informa√ß√£o do usu√°rio
      scanf("%s" , cpf); //%s refere- se a string
      
     strcpy(arquivo, cpf); //Responsavel por copiar os valores das string
      
      FILE *file; // cria o arquivo
      file = fopen(arquivo,"w"); // cria o arquivo e o "w" significa escrever
-     fprintf(file,cpf); // salvo o valor da vari·vel
+     fprintf(file,cpf); // salvo o valor da vari√°vel
      fclose(file); // fecha o arquivo
      
      file = fopen(arquivo,"a");
@@ -75,26 +75,25 @@ int consulta()
     
     if(file == NULL);
     {
-    	printf( "N„o foi possivel abrir o arquivo, n„o localizado!.\n");
+    	printf( "N√£o foi possivel abrir o arquivo, n√£o localizado!.\n");
     }
   
     while(fgets(conteudo, 200, file) != NULL)
     {
-	   printf("\nEssas s„o as informaÁıes do usu·rio: ");
+	   printf("\nEssas s√£o as informa√ß√µes do usu√°rio: ");
 	   printf("%s", conteudo);
 	   printf("\n\n");
 	   }  
 
        system("pause");
-
-
+	
 }
 
 int deletar()
 {
 	char cpf[40];
 	
-	printf("Digite o CPF do usu·rio a ser deletado: ");
+	printf("Digite o CPF do usu√°rio a ser deletado: ");
 	scanf("%s", cpf);
 	
     remove(cpf);
@@ -104,7 +103,7 @@ int deletar()
 	
 	if(file == NULL)
 	{
-		printf("O usu·rio n„o se encontra no sistema!.\n");
+		printf("O usu√°rio n√£o se encontra no sistema!.\n");
 		system("pause");
 	
 	}
@@ -113,7 +112,7 @@ int deletar()
 
 int main()
 {
-	int opcao=0;//Definindo vari·veis
+	int opcao=0;//Definindo vari√°veis
 	int laco=1;
 	
 	for(laco=1;laco=1;)
@@ -123,27 +122,27 @@ system("cls");
  
 setlocale(LC_ALL,"Portuguese");// Definindo a linguagem
 
-printf("### CartÛrio da EBAC###\n\n");// Inicio do menu
-printf("Escolha a opÁ„o desejada do menu:\n\n");
+printf("### Cart√≥rio da EBAC###\n\n");// Inicio do menu
+printf("Escolha a op√ß√£o desejada do menu:\n\n");
 printf("\t1 - Registrar nomes\n");
 printf("\t2 - Consultar nomes\n");
 printf("\t3 - Deletar nomes\n\n");	
 printf("\t4 - Sair do sistema\n\n");
-printf("OpÁ„o:");// fim do menu
+printf("Op√ß√£o:");// fim do menu
 
-scanf("%d", &opcao);// Armazenando a escolha do usu·rio
+scanf("%d", &opcao);// Armazenando a escolha do usu√°rio
 
 system("cls"); // responsavel por limpar a tela
 
-switch(opcao) // inicio da seleÁ„o do menu
+switch(opcao) // inicio da sele√ß√£o do menu
  {
   case1:
-       registro(); // chamada de funÁıes
+       registro(); // chamada de fun√ß√µes
        break;
 
 case 2:
 	consulta();
-    break;
+        break;
 
 case 3:
     deletar();
@@ -155,33 +154,8 @@ case 3:
     	break;
 
  default:
- 	printf("Essa opÁ„o n„o est· dÌsponÌvel""\n");
+ 	printf("Essa op√ß√£o n√£o est√° d√≠spon√≠vel""\n");
 	system("pause");
 	break;
-  // fim da seleÁ„o
+  // fim da sele√ß√£o
  }
-
-
-if(opcao==1)// Inicio da seleÁ„o
-
-{
-   printf("VocÍ escolheu o registro de nome!");
-   system("pause");
-}
-    if(opcao==2)
-{
- printf("VocÍ escolheu consultar os nomes!");
- system("pause");
-}
-if(opcao==3)
-{
-	printf("VocÍ escolheu deletar nomes!");
-    system("pause");
-}
-   if(opcao==4)
-{
-	printf("Essa opÁ„o n„o est· dÌsponÌvel");
-	system("pause");
-	
-    // Fim da seleÁ„o
-   
